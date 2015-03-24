14257442a4b6ab573e87a2edc8ed922e9de36bbc
Johannes Laire <johannes@laire.fi>
2015-03-24 18:41:40 +0100

    Tabs to spaces

--- src/PostalCodes/GenericPostalCodes/DefaultPostalCode.cs
+++ src/PostalCodes/GenericPostalCodes/DefaultPostalCode.cs
@@ -3,74 +3,74 @@ using System.Text.RegularExpressions;
 
 namespace PostalCodes.GenericPostalCodes
 {
-	internal class DefaultPostalCode : AlphaNumericPostalCode {
+    internal class DefaultPostalCode : AlphaNumericPostalCode {
 
-		public DefaultPostalCode(string postalCode) : base(_formats, postalCode, true) 
-		{
-		}
+        public DefaultPostalCode(string postalCode) : base(_formats, postalCode, true)
+        {
+        }
 
-		public DefaultPostalCode(string postalCode, bool allowConvertToShort) : base(_formats, postalCode, allowConvertToShort) 
-		{
-		}
+        public DefaultPostalCode(string postalCode, bool allowConvertToShort) : base(_formats, postalCode, allowConvertToShort)
+        {
+        }
 
-		protected override PostalCode PredecessorImpl
-		{
-			get 
-			{ 
-				var b = GenerateSuccesorOrPredecessor(GetInternalValue(), false);
-				if (b == null) 
-				{
-					return null;
-				}
-				return new DefaultPostalCode (b);
-			}
-		}
+        protected override PostalCode PredecessorImpl
+        {
+            get
+            {
+                var b = GenerateSuccesorOrPredecessor(GetInternalValue(), false);
+                if (b == null)
+                {
+                    return null;
+                }
+                return new DefaultPostalCode (b);
+            }
+        }
 
-		protected override PostalCode SuccessorImpl
-		{
-			get 
-			{ 
-				var b = GenerateSuccesorOrPredecessor(GetInternalValue(), true);
-				if (b == null) 
-				{
-					return null;
-				}
-				return new DefaultPostalCode (b);
-			}
-		}
-		public override PostalCode ExpandPostalCodeAsLowestInRange ()
-		{
-			if (_currentFormatType == FormatType.Short) {
-				if (_currentFormat.ShortExpansionAsLowestInRange != null) {
-					return new DefaultPostalCode (ToString () + _currentFormat.ShortExpansionAsLowestInRange);
-				} else {
-					throw new ArgumentException ("Requested short postal code expansion but no expansion provided (lowest)");
-				}
-			}
+        protected override PostalCode SuccessorImpl
+        {
+            get
+            {
+                var b = GenerateSuccesorOrPredecessor(GetInternalValue(), true);
+                if (b == null)
+                {
+                    return null;
+                }
+                return new DefaultPostalCode (b);
+            }
+        }
+        public override PostalCode ExpandPostalCodeAsLowestInRange ()
+        {
+            if (_currentFormatType == FormatType.Short) {
+                if (_currentFormat.ShortExpansionAsLowestInRange != null) {
+                    return new DefaultPostalCode (ToString () + _currentFormat.ShortExpansionAsLowestInRange);
+                } else {
+                    throw new ArgumentException ("Requested short postal code expansion but no expansion provided (lowest)");
+                }
+            }
 
-			return new PTPostalCode(ToString());
-		}
+            return new PTPostalCode(ToString());
+        }
 
-		public override PostalCode ExpandPostalCodeAsHighestInRange ()
-		{
-			if (_currentFormatType == FormatType.Short) {
-				if (_currentFormat.ShortExpansionAsHighestInRange != null) {
-					return new DefaultPostalCode (ToString () + _currentFormat.ShortExpansionAsHighestInRange);
-				} else {
-					throw new ArgumentException ("Requested short postal code expansion but no expansion provided (highest)");
-				}
-			}
+        public override PostalCode ExpandPostalCodeAsHighestInRange ()
+        {
+            if (_currentFormatType == FormatType.Short) {
+                if (_currentFormat.ShortExpansionAsHighestInRange != null) {
+                    return new DefaultPostalCode (ToString () + _currentFormat.ShortExpansionAsHighestInRange);
+                } else {
+                    throw new ArgumentException ("Requested short postal code expansion but no expansion provided (highest)");
+                }
+            }
 
-			return new PTPostalCode(ToString());
-		}
+            return new PTPostalCode(ToString());
+        }
 
-		private static PostalCodeFormat[] _formats = new PostalCodeFormat[] {
-			new PostalCodeFormat {
-				Name = "Default Alpha Numeric Format",
-				OutputDefault = "xxxxxxxx",
-				RegexDefault = new Regex("^[A-Z0-9]{1,15}$", RegexOptions.Compiled),
-			}
-		};
-	}
+        private static PostalCodeFormat[] _formats = new PostalCodeFormat[] {
+            new PostalCodeFormat {
+                Name = "Default Alpha Numeric Format",
+                OutputDefault = "xxxxxxxx",
+                RegexDefault = new Regex("^[A-Z0-9]{1,15}$", RegexOptions.Compiled),
+            }
+        };
+    }
     
 }
