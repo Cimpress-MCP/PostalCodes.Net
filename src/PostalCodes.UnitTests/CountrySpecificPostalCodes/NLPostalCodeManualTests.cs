94368fcfdda60b3cb9df422d9e637112237f4240
Pavel Šavara <pavel.savara@cimpress.com>
2019-12-18 15:31:49 +0100

    Revert "Fixed Dutch postal code validation. (#74)"
    This reverts commit a46a9d7a8fccd4f1213051730be1bdb11cb65eab.

--- src/PostalCodes.UnitTests/CountrySpecificPostalCodes/NLPostalCodeManualTests.cs
+++ src/PostalCodes.UnitTests/CountrySpecificPostalCodes/NLPostalCodeManualTests.cs
@@ -8,6 +8,8 @@ namespace PostalCodes.UnitTests.CountrySpecificPostalCodes
         [Test]
         [TestCase("9992 ZZ", "9992ZY")]
         [TestCase("9992", "9991")]
+        [TestCase("1000", "0999")]
+        [TestCase("1000 AA", "0999ZZ")]
         [TestCase("4000 ZZ", "4000ZY")]
         [TestCase("4000", "3999")]
         [TestCase("1422", "1421")]
@@ -29,6 +31,14 @@ namespace PostalCodes.UnitTests.CountrySpecificPostalCodes
         public void Successor_ValidInput_ReturnsCorrectPostalCode(string postalCode, string postalCodeSuccessor)
         {
             Assert.AreEqual(postalCodeSuccessor, (new NLPostalCode(postalCode)).Successor.ToString());
+        }
+
+        [Test]
+        [TestCase("0000 AA")]
+        [TestCase("0000")]
+        public void Predecessor_FirstInRange_ReturnsNull(string postalCode)
+        {
+            Assert.IsNull((new NLPostalCode(postalCode)).Predecessor);
         }
 
         [Test]
